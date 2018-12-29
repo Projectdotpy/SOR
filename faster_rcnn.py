f2f0a20586555545e71d3bd632027f066815ef97
spallas <davide.spallaccini@gmail.com>
2018-12-29 09:23:44 +0100

    Add calculate iou in FasterRCNN class

--- faster_rcnn.py
+++ faster_rcnn.py
@@ -41,16 +41,16 @@ def iou(a, b):
 
 
 def get_new_img_size(width, height, img_min_side=300):
-        if width <= height:
-            f = float(img_min_side) / width
-            resized_height = int(f * height)
-            resized_width = img_min_side
-        else:
-            f = float(img_min_side) / height
-            resized_width = int(f * width)
-            resized_height = img_min_side
+    if width <= height:
+        f = float(img_min_side) / width
+        resized_height = int(f * height)
+        resized_width = img_min_side
+    else:
+        f = float(img_min_side) / height
+        resized_width = int(f * width)
+        resized_height = img_min_side
 
-        return resized_width, resized_height
+    return resized_width, resized_height
 
 
 class Config:
@@ -506,7 +506,8 @@ class FasterRCNN:
                         idx, 2] + n_anchratios *
                     best_anchor_for_bbox[idx, 3]] = 1
                 start = 4 * (best_anchor_for_bbox[idx, 2] + n_anchratios * best_anchor_for_bbox[idx, 3])
-                y_rpn_regr[best_anchor_for_bbox[idx, 0], best_anchor_for_bbox[idx, 1], start:start + 4] = best_dx_for_bbox[idx, :]
+                y_rpn_regr[best_anchor_for_bbox[idx, 0], best_anchor_for_bbox[idx, 1],
+                start:start + 4] = best_dx_for_bbox[idx, :]
 
         y_rpn_overlap = np.transpose(y_rpn_overlap, (2, 0, 1))
         y_rpn_overlap = np.expand_dims(y_rpn_overlap, axis=0)
@@ -822,6 +823,109 @@ class FasterRCNN:
             print(e)
             return x, y, w, h
 
+    def calc_iou(self, R, img_data, C, class_mapping):
+        """Converts from (x1,y1,x2,y2) to (x,y,w,h) format
+
+        Args:
+            R: bboxes, probs
+        """
+        bboxes = img_data['bboxes']
+        (width, height) = (img_data['width'], img_data['height'])
+        # get image dimensions for resizing
+        (resized_width, resized_height) = get_new_img_size(width, height, C.im_size)
+
+        gta = np.zeros((len(bboxes), 4))
+
+        for bbox_num, bbox in enumerate(bboxes):
+            # get the GT box coordinates, and resize to account for image resizing
+            # gta[bbox_num, 0] = (40 * (600 / 800)) / 16 = int(round(1.875)) = 2 (x in feature map)
+            gta[bbox_num, 0] = int(round(bbox['x1'] * (resized_width / float(width)) / C.rpn_stride))
+            gta[bbox_num, 1] = int(round(bbox['x2'] * (resized_width / float(width)) / C.rpn_stride))
+            gta[bbox_num, 2] = int(round(bbox['y1'] * (resized_height / float(height)) / C.rpn_stride))
+            gta[bbox_num, 3] = int(round(bbox['y2'] * (resized_height / float(height)) / C.rpn_stride))
+
+        x_roi = []
+        y_class_num = []
+        y_class_regr_coords = []
+        y_class_regr_label = []
+        IoUs = []  # for debugging only
+
+        # R.shape[0]: number of bboxes (=300 from non_max_suppression)
+        for ix in range(R.shape[0]):
+            (x1, y1, x2, y2) = R[ix, :]
+            x1 = int(round(x1))
+            y1 = int(round(y1))
+            x2 = int(round(x2))
+            y2 = int(round(y2))
+
+            best_iou = 0.0
+            best_bbox = -1
+            # Iterate through all the ground-truth bboxes to calculate the iou
+            for bbox_num in range(len(bboxes)):
+                curr_iou = iou([gta[bbox_num, 0], gta[bbox_num, 2], gta[bbox_num, 1], gta[bbox_num, 3]],
+                               [x1, y1, x2, y2])
+
+                # Find out the corresponding ground-truth bbox_num with larget iou
+                if curr_iou > best_iou:
+                    best_iou = curr_iou
+                    best_bbox = bbox_num
+
+            if best_iou < C.classifier_min_overlap:
+                continue
+            else:
+                w = x2 - x1
+                h = y2 - y1
+                x_roi.append([x1, y1, w, h])
+                IoUs.append(best_iou)
+
+                if C.classifier_min_overlap <= best_iou < C.classifier_max_overlap:
+                    # hard negative example
+                    cls_name = 'bg'
+                elif C.classifier_max_overlap <= best_iou:
+                    cls_name = bboxes[best_bbox]['class']
+                    cxg = (gta[best_bbox, 0] + gta[best_bbox, 1]) / 2.0
+                    cyg = (gta[best_bbox, 2] + gta[best_bbox, 3]) / 2.0
+
+                    cx = x1 + w / 2.0
+                    cy = y1 + h / 2.0
+
+                    tx = (cxg - cx) / float(w)
+                    ty = (cyg - cy) / float(h)
+                    tw = np.log((gta[best_bbox, 1] - gta[best_bbox, 0]) / float(w))
+                    th = np.log((gta[best_bbox, 3] - gta[best_bbox, 2]) / float(h))
+                else:
+                    print('roi = {}'.format(best_iou))
+                    raise RuntimeError
+
+            class_num = class_mapping[cls_name]
+            class_label = len(class_mapping) * [0]
+            class_label[class_num] = 1
+            y_class_num.append(copy.deepcopy(class_label))
+            coords = [0] * 4 * (len(class_mapping) - 1)
+            labels = [0] * 4 * (len(class_mapping) - 1)
+            if cls_name != 'bg':
+                label_pos = 4 * class_num
+                sx, sy, sw, sh = C.classifier_regr_std
+                coords[label_pos:4 + label_pos] = [sx * tx, sy * ty, sw * tw, sh * th]
+                labels[label_pos:4 + label_pos] = [1, 1, 1, 1]
+                y_class_regr_coords.append(copy.deepcopy(coords))
+                y_class_regr_label.append(copy.deepcopy(labels))
+            else:
+                y_class_regr_coords.append(copy.deepcopy(coords))
+                y_class_regr_label.append(copy.deepcopy(labels))
+
+        if len(x_roi) == 0:
+            return None, None, None, None
+
+        # bboxes that iou > C.classifier_min_overlap for all gt bboxes in 300 non_max_suppression bboxes
+        X = np.array(x_roi)
+        # one hot code for bboxes from above => x_roi (X)
+        Y1 = np.array(y_class_num)
+        # corresponding labels and corresponding gt bboxes
+        Y2 = np.concatenate([np.array(y_class_regr_label), np.array(y_class_regr_coords)], axis=1)
+
+        return np.expand_dims(X, axis=0), np.expand_dims(Y1, axis=0), np.expand_dims(Y2, axis=0), IoUs
+
     def rpn_to_roi(self, rpn_layer, regr_layer, C, dim_ordering, use_regr=True, max_boxes=300, overlap_thresh=0.9):
         """Convert rpn layer to roi bboxes
 
@@ -920,7 +1024,8 @@ class FasterRCNN:
 
         # Apply non_max_suppression
         # Only extract the bboxes. Don't need rpn probs in the later process
-        result = self.non_max_suppression_fast(all_boxes, all_probs, overlap_thresh=overlap_thresh, max_boxes=max_boxes)[0]
+        result = \
+        self.non_max_suppression_fast(all_boxes, all_probs, overlap_thresh=overlap_thresh, max_boxes=max_boxes)[0]
 
         return result
 
