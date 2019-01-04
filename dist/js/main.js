c29b362f6876dab94561e5a0c639a26c3c601a6a
spallas <davide.spallaccini@gmail.com>
2019-01-04 13:08:33 +0100

    Add drag and drop

---
+++ dist/js/main.js
@@ -0,0 +1,55 @@
+// ************************ Drag and drop ***************** //
+let dropArea = document.getElementById("drop-area")
+
+// Prevent default drag behaviors
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, preventDefaults, false)
+    document.body.addEventListener(eventName, preventDefaults, false)
+})
+
+// Highlight drop area when item is dragged over it
+['dragenter', 'dragover'].forEach(eventName => {
+    dropArea.addEventListener(eventName, highlight, false)
+})
+
+['dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, unhighlight, false)
+})
+
+// Handle dropped files
+dropArea.addEventListener('drop', handleDrop, false)
+
+function preventDefaults(e) {
+    e.preventDefault()
+    e.stopPropagation()
+}
+
+function highlight(e) {
+    dropArea.classList.add('highlight')
+}
+
+function unhighlight(e) {
+    dropArea.classList.remove('active')
+}
+
+function handleDrop(e) {
+    var dt = e.dataTransfer
+    var files = dt.files
+    document.getElementById('fileElem').files = files
+}
+
+function handleFiles(files) {
+    console.log(files)
+    previewFile(files[0])
+    document.getElementById("form_id").submit();
+}
+
+function previewFile(file) {
+    let reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onloadend = function () {
+        let img = document.createElement('img')
+        img.src = reader.result
+        document.getElementById('gallery').appendChild(img)
+    }
+}
